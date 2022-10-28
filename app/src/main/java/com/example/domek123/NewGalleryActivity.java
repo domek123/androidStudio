2d52bb52061f08b19e0662b13ac056dfeeb7c6f4
Dominik Wójcik <dwojcik@x-formation.com>
2022-10-29 01:27:01 +0200

    refactor defaultAdapte, delete photos

--- app/src/main/java/com/example/domek123/NewGalleryActivity.java
+++ app/src/main/java/com/example/domek123/NewGalleryActivity.java
@@ -3,9 +3,14 @@ package com.example.domek123;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.ListView;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NewGalleryActivity extends AppCompatActivity {
     private ListView listView1;
@@ -13,13 +18,20 @@ public class NewGalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_gallery);
-        ArrayList<String> list = new ArrayList();
-        list.add("a");
-        list.add("b");
+        Bundle bundle = getIntent().getExtras();
+        String folder = bundle.getString("folderName");
+        File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File dir = new File(pic, "DominikWojcik/" + folder);
+        dir.mkdir();
+        Log.d("AAA",dir.listFiles().length + " ");
+        ArrayList<File> list = new ArrayList<>(
+                Arrays.asList(dir.listFiles())
+        );
+        Log.d("AAA",list.size() + " ");
         listView1 = findViewById(R.id.listViewID);
         TestAdapter adapter = new TestAdapter (
                 NewGalleryActivity.this,
-                R.layout.listview,
+                R.layout.gallery_item,
                 list
         );
         listView1.setAdapter(adapter);
